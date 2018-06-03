3b81c0fa0359216688045ae0732931dd5db2e1af
Ahmed Elbasha <33283612+Ahmed-Elbasha@users.noreply.github.com>
2018-06-04 01:49:29 +0200

    Finished the UI Design of OrderViewController

---
+++ CoffeeShop/CoffeeShop/OrderViewController.swift
@@ -0,0 +1,25 @@
+//
+//  OrderViewController.swift
+//  CoffeeShop
+//
+//  Created by Ahmed Elbasha on 6/3/18.
+//  Copyright © 2018 Ahmed Elbasha. All rights reserved.
+//
+
+import UIKit
+
+class OrderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
