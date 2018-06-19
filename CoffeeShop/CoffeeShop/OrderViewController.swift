1eb41db7ff0d783a3cf83b7fba22b1d1ec689ada
Ahmed Elbasha <33283612+Ahmed-Elbasha@users.noreply.github.com>
2018-06-19 16:56:39 +0200

    OrderViewController is wired up

--- CoffeeShop/CoffeeShop/OrderViewController.swift
+++ CoffeeShop/CoffeeShop/OrderViewController.swift
@@ -9,7 +9,14 @@
 import UIKit
 
 class OrderViewController: UIViewController {
-
+    
+    @IBOutlet weak var customerNameTextField: UITextField!
+    @IBOutlet weak var addChocolateSwitch: UISwitch!
+    @IBOutlet weak var addWhippedCreamSwitch: UISwitch!
+    @IBOutlet weak var quantityStepper: UIStepper!
+    @IBOutlet weak var quantityTextValue: UITextField!
+    @IBOutlet weak var orderButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,9 @@ class OrderViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func orderButtonPressed(_ sender: Any) {
+        print("order button was pressed")
+    }
+    
 }
 
